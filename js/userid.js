5edd9d283c26e2b4707c22452a1493976595b760
uehrmdr <99104704+uehrmdr@users.noreply.github.com>
2022-06-15 10:10:39 +0900

    login関数追加
    login関数追加

---
+++ js/userid.js
@@ -0,0 +1,17 @@
+function login(userid){
+  
+//今日の日付データを変数hidukeに格納
+var hiduke=new Date(); 
+var date = new Date().getTime();
+var timest = new Date(date).toString();
+//年・月・日を取得
+var year = hiduke.getFullYear();
+var month = hiduke.getMonth()+1;
+var day = hiduke.getDate();
+//時間を取得
+var time = timest.split(' ')[4]
+var kyou = year+"_"+month+"_"+day;
+var userid = "test_"+kyou+"_"+time;
+
+return userid
+}
