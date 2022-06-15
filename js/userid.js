ce54bdde6ddeeed05fa3149f22ae6a6a5583fec9
uehrmdr <99104704+uehrmdr@users.noreply.github.com>
2022-06-15 10:27:02 +0900

    user_id実行

--- js/userid.js
+++ js/userid.js
@@ -1,17 +1,18 @@
-function login(userid){
+(function login(){
   
-//今日の日付データを変数hidukeに格納
-var hiduke=new Date(); 
-var date = new Date().getTime();
-var timest = new Date(date).toString();
-//年・月・日を取得
-var year = hiduke.getFullYear();
-var month = hiduke.getMonth()+1;
-var day = hiduke.getDate();
-//時間を取得
-var time = timest.split(' ')[4]
-var kyou = year+"_"+month+"_"+day;
-var userid = "test_"+kyou+"_"+time;
-
-return userid
-}
+  //今日の日付データを変数hidukeに格納
+  var hiduke=new Date(); 
+  var date = new Date().getTime();
+  var timest = new Date(date).toString();
+  //年・月・日を取得
+  var year = hiduke.getFullYear();
+  var month = hiduke.getMonth()+1;
+  var day = hiduke.getDate();
+  //時間を取得
+  var time = timest.split(' ')[4]
+  var kyou = year+"_"+month+"_"+day;
+  dataLayer.push({
+    'event': 'userdata_track',
+    'user_id': "test_"+kyou+"_"+time
+    });
+  })()
